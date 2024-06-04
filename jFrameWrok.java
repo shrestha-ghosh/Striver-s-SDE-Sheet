4133efb3a7eba1e0ad55b761f0338d3ad7b5681f
Shrestha Ghosh <132203056+shresthaghosh06@users.noreply.github.com>
2024-06-04 10:37:42 +0530

    Stack using Java collection Framework

---
+++ jFrameWrok.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class  jFrameWrok{
+    public static void main(String args[]){
+        Stack<Integer> st = new Stack<>();
+
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+
+        while(!st.isEmpty())  {
+            System.out.println(st.peek());
+            st.pop();
+        }
+    }
+    
+}
