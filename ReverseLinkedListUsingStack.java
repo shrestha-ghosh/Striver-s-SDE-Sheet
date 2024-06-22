616df3b9e95b7d73cb65e9d73e0fdcc255307df1
Shrestha Ghosh <132203056+shresthaghosh06@users.noreply.github.com>
2024-06-22 23:23:04 +0530

    Day 5 🚀

---
+++ ReverseLinkedListUsingStack.java
@@ -0,0 +1,62 @@
+import java.util.Stack;
+
+class Node {
+    int data;      
+    Node next;     
+
+
+    Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+public class ReverseLinkedListUsingStack {
+
+    public static Node reverseLinkedList(Node head) {
+        
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        // Recursive step:
+        // Reverse the linked list starting from the second node (head.next).
+        Node newHead = reverseLinkedList(head.next);
+        
+        Node front = head.next;
+        
+        front.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public static void printLinkedList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(3);
+        head.next.next = new Node(2);
+        head.next.next.next = new Node(4);
+
+        System.out.print("Original Linked List: ");
+        printLinkedList(head);
+
+        // Reverse the linked list
+        head = reverseLinkedList(head);
+        System.out.print("Reversed Linked List: ");
+        printLinkedList(head);
+    }
+}
