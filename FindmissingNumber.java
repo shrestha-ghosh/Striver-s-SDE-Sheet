630b986b75fb711fe03cecd25814b6735cc5e7c4
Shrestha Ghosh <132203056+shresthaghosh06@users.noreply.github.com>
2024-06-29 17:35:47 +0530

    Day 8 🚀

---
+++ FindmissingNumber.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class FindmissingNumber {
+    public static int missingNumber(int []a, int N) {
+        int sum = (N * (N + 1)) / 2;
+        int s2 = 0;
+        for (int i = 0; i < N - 1; i++) {
+            s2 += a[i];
+        }
+
+        int missingNum = sum - s2;
+        return missingNum;
+    }
+
+    public static void main(String args[]) {
+        int N = 5;
+        int a[] = {1, 2, 4, 5};
+
+        int ans = missingNumber(a, N);
+        System.out.println("The missing number is: " + ans);
+    }
+}
